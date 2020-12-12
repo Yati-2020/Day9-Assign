1e43ab2a227d3a1de8d783841e64189233f42b86
yatiP <yati.patil17@gmail.com>
2020-12-12 16:25:54 +0530

    Adding empWage to store Daily and Total Wage

---
+++ empDailyTotalWage.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+IS_FULL_TIME=1
+IS_PART_TIME=2
+salary=0
+RATE_PER_HR=20
+MAX_WORKING_DAYS=2;
+MAX_WORKING_HRS=10;
+
+#VARIABLES
+totalWorkingDays=0;
+totalWorkingHrs=0;
+
+function getWorkingHrs()
+{
+        case $empCheck in
+                $IS_FULL_TIME)
+                empHrs=8
+                ;;
+                $IS_PART_TIME)
+                empHrs=4
+                ;;
+                *)
+                empHrs=0
+                ;;
+        esac
+        echo $empHrs
+}
+
+while [[ $totalWorkingHrs -lt $MAX_WORKING_HRS && $totalWorkingDays -lt $MAX_WORKING_DAYS ]]
+do
+        ((totalWorkingDays++))
+        empCheck=$((RANDOM%3))
+        empHrs="$( getWorkingHrs $empCheck )"
+        dailyWage=$(( $empHrs*$RATE_PER_HR ))
+        dailyWageArray[$totalWorkingDays]=$dailyWage
+        totalWorkingHrs=$(( $totalWorkingHrs+$empHrs ))
+done
+ totalSalary=$(($totalWorkingHrs*$RATE_PER_HR))
+echo "Array elements" ${dailyWageArray[@]}
+echo "Index" ${!dailyWageArray[@]}
+
+echo "Employee Wage per month:" $totalSalary
