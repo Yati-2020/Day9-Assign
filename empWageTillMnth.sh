19abe6bc5b77bf9589932241dd4412f3d81fd006
yatiP <yati.patil17@gmail.com>
2020-12-12 16:07:42 +0530

    Adding empWage reached for month

---
+++ empWageTillMnth.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+isFullTime=1
+isPartTime=2
+salary=0
+ratePerHr=20
+numbOfWorkingDays=20;
+
+for ((day=1;day<=$numbOfWorkingDays;day++))
+do
+
+        empCheck=$((RANDOM%3))
+        case $empCheck in
+                $isFullTime)
+                echo "FullTime Employee"
+                empHrs=8
+                ;;
+                $isPartTime)
+                echo "PartTime Employee"
+                empHrs=4
+                ;;
+                *)
+                echo "Employee is Absent"
+                empHrs=0
+                ;;
+        esac
+
+        salary=$(( $ratePerHr*$empHrs ))
+        echo "Per Day Salary:" $salary
+        totalSalary=$(($totalSalary+$salary))
+done
+echo "Employee Wage per month:" $totalSalary
