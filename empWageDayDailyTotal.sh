f6a60938bc044a65bf0812690d162165af98d489
yatiP <yati.patil17@gmail.com>
2020-12-12 16:33:50 +0530

    Adding empWage to store Daily, Day and total wage

---
+++ empWageDayDailyTotal.sh
@@ -0,0 +1,46 @@
+#!/bin/bash 
+
+#CONSTANTS
+IS_FULL_TIME=1
+IS_PART_TIME=2
+salary=0
+RATE_PER_HR=20
+MAX_WORKING_DAYS=5;
+MAX_WORKING_HRS=40;
+
+#VARIABLES
+totalWorkingDays=0;
+totalWorkingHrs=0;
+
+declare -A dailyWageDict
+
+function getWorkingHrs()
+{
+        case $empCheck in
+                $IS_FULL_TIME)
+                empHrs=8
+                ;;
+                $IS_PART_TIME)
+                empHrs=4
+                ;;
+                *)
+                empHrs=0
+                ;;
+        esac
+        echo $empHrs
+}
+
+while [[ $totalWorkingHrs -lt $MAX_WORKING_HRS && $totalWorkingDays -lt $MAX_WORKING_DAYS ]]
+do
+        ((totalWorkingDays++))
+        empCheck=$((RANDOM%3))
+        empHrs="$( getWorkingHrs $empCheck )"
+        dailyWage=$(( $empHrs*$RATE_PER_HR ))
+        dailyWageDict["Day"$totalWorkingDays]=$dailyWage
+        totalWorkingHrs=$(( $totalWorkingHrs+$empHrs ))
+done
+ totalSalary=$(($totalWorkingHrs*$RATE_PER_HR))
+echo "Dictionary elements" ${dailyWageArray[@]}
+echo "Keys" ${!dailyWageDict[@]}
+
+echo "Employee Wage per month:" $totalSalary
